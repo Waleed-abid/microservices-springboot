3b75cad3be98031dd2a38c441b89e3f51805ce8c
Waleed-abid <waleedmain9@gmail.com>
2023-02-10 18:50:10 +0500

    added fraud feature

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,18 +1,31 @@
 package com.amigoscode.customer;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService{
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest request) {
             Customer customer = Customer.builder()
                     .firstName(request.firstName())
                     .lastName(request.lastName())
                     .email(request.email())
                     .build();
+        customerRepository.saveAndFlush(customer);
 
             //todo validation checks
-
-        customerRepository.save(customer);
+       FraudCheckResponse fraudCheckResponse =  restTemplate.getForObject(
+          "localhost:8081/api/v1/fraud-check/{customerId}",
+          FraudCheckResponse.class,
+                customer.getId()
+        );
+    if(fraudCheckResponse.isFraudster()){
+        throw new IllegalStateException("fraudster");
+    }
     }
 }
